19e09b95157fb26361263f50c0ae64e5d97dcc2c
Cyril David <cdhinho27370@outlook.fr>
2026-04-10 15:49:47 +0200

    page de détail de la phrase du jour

--- app/src/Controller/SentenceController.php
+++ app/src/Controller/SentenceController.php
@@ -2,12 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Sentence;
 use App\Repository\SentenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class SentenceController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(SentenceRepository $sentenceRepository): Response
@@ -16,6 +17,13 @@ final class HomeController extends AbstractController
         
         return $this->render('home/index.html.twig', [
             'sentences' => $sentences
+        ]);
+    }
+    #[Route('/sentence/{id}', name: 'app_sentence_show')]
+    public function show(Sentence $sentence): Response
+    {
+        return $this->render('sentence/show.html.twig', [
+            'sentence' => $sentence,
         ]);
     }
 }
