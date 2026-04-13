874c55b1034eb3f15fc2ac994334531562ee8174
Cyril David <cdhinho27370@outlook.fr>
2026-04-13 10:15:38 +0200

    formulaire de commentaire

--- app/src/Controller/SentenceController.php
+++ app/src/Controller/SentenceController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Sentence;
+use App\Form\CommentType;
 use App\Repository\CommentRepository;
 use App\Repository\SentenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -21,25 +24,43 @@ final class SentenceController extends AbstractController
             'sentences' => $sentences
         ]);
     }
+
     #[Route('/sentence/{id}', name: 'app_sentence_show')]
-    public function show(Sentence $sentence, CommentRepository $commentRepository): Response
+    public function show(Sentence $sentence, CommentRepository $commentRepository, Request $request, EntityManagerInterface $entityManager): Response
     {   
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment); // manquait $comment
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setAuthor($this->getUser());
+            $comment->setCreatedAt(new \DateTimeImmutable()); // DateTimeImmutable pas DateTime
+            $comment->setSentence($sentence);
+
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_sentence_show', ['id' => $sentence->getId()]);
+        }
+
         $comments = $commentRepository->findBy(
             ['sentence' => $sentence], 
-            ['createdAt' => 'DESC']);
+            ['createdAt' => 'DESC']
+        );
         
-            return $this->render('sentence/show.html.twig', [
+        return $this->render('sentence/show.html.twig', [
             'sentence' => $sentence,
-            'comments' => $comments
+            'comments' => $comments,
+            'commentForm' => $form, // manquait le formulaire
         ]);
     }
 
     #[Route('/sentence/{id}/like', name: 'app_sentence_like')]
     public function like(Sentence $sentence, EntityManagerInterface $entityManager): Response
     {
-    $sentence->setLikes($sentence->getLikes() + 1);
-    $entityManager->flush();
+        $sentence->setLikes($sentence->getLikes() + 1);
+        $entityManager->flush();
 
-    return $this->redirectToRoute('app_sentence_show', ['id' => $sentence->getId()]);
+        return $this->redirectToRoute('app_sentence_show', ['id' => $sentence->getId()]);
     }
 }
